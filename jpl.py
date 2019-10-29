8d0087a75f104311a0e5eecb93986bdd3e47f946
Jonas Ampferl <Jonas@ampferl.com>
2019-10-29 14:12:13 +0100

    Fix some Error Message Bugs

--- jpl.py
+++ jpl.py
@@ -279,20 +279,24 @@ class ParseResult:
 	def __init__(self):
 		self.error = None
 		self.node = None
+		self.advance_count = 0
+
+	def register_advancment(self):
+		self.advance_count += 1
 
 	def register(self, res):
-		if isinstance(res, ParseResult):
-			if res.error: self.error = res.error
-			return res.node
-
-		return res
+		self.advance_count += res.advance_count
+		if res.error: self.error = res.error
+		return res.node
 
 	def success(self, node):
 		self.node = node
 		return self
 
 	def failure(self, error):
-		self.error = error
+		if not self.error or self.advance_count == 0:
+
+			self.error = error
 		return self
 
 #######################################
@@ -326,20 +330,24 @@ class Parser:
 		res = ParseResult()
 		tok = self.current_tok
 		if tok.type in (TT_INT, TT_FLOAT):
-			res.register(self.advance())
+			res.register_advancment()
+			self.advance()
 			return res.success(NumberNode(tok))
 		
 		elif tok.type == TT_IDENTIFIER:
-			res.register(self.advance())
+			res.register_advancment()
+			self.advance()
 			return res.success(VarAccessNode(tok))
 
 		elif tok.type == TT_LPAREN:
-			res.register(self.advance())
+			res.register_advancment()
+			self.advance()
 			expr = res.register(self.expr())
 			if res.error: 
 				return res
 			if self.current_tok.type == TT_RPAREN:
-				res.register(self.advance())
+				res.register_advancment()
+				self.advance()
 				return res.success(expr)
 			else:
 				return res.failure(InvalidSyntaxError(
@@ -349,7 +357,7 @@ class Parser:
 
 		return res.failure(InvalidSyntaxError(
 			tok.pos_start, tok.pos_end,
-			"Expected int, float, '+', '-' or '('"
+			"Expected int, float, identifier, '+', '-' or '('"
 		))
 	
 	def power(self):
@@ -360,7 +368,8 @@ class Parser:
 		tok = self.current_tok
 
 		if tok.type in (TT_PLUS, TT_MINUS):
-			res.register(self.advance())
+			res.register_advancment()
+			self.advance()
 			factor = res.register(self.factor())
 			if res.error: return res
 			return res.success(UnaryOpNode(tok, factor))
@@ -374,7 +383,8 @@ class Parser:
 		res = ParseResult()
 
 		if self.current_tok.matches(TT_KEYWORD, 'var'):
-			res.register(self.advance())
+			res.register_advancment()
+			self.advance()
 
 			if self.current_tok.type != TT_IDENTIFIER:
 				return res.failure(InvalidSyntaxError(
@@ -383,7 +393,8 @@ class Parser:
 				))
 
 			var_name = self.current_tok
-			res.register(self.advance())
+			res.register_advancment()
+			self.advance()
 
 			if self.current_tok.type != TT_EQ:
 				return res.failure(InvalidSyntaxError(
@@ -391,13 +402,22 @@ class Parser:
 					"Expected '='"
 				))
 
-			res.register(self.advance())
+			res.register_advancment()
+			self.advance()
 			expr = res.register(self.expr())
 			if res.error: return res
 			return res.success(VarAssignNode(var_name, expr))
 
 
-		return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
+		node = res.register(self.bin_op(self.term, (TT_PLUS, TT_MINUS)))
+
+		if res.error: 
+			return res.failure(InvalidSyntaxError(
+				self.current_tok.pos_start, self.current_tok.pos_end,
+				"Expected 'var', int, float, identifier, '+', '-' or '('"
+			))
+
+		return res.success(node)
 
 	###################################
 
@@ -410,7 +430,8 @@ class Parser:
 
 		while self.current_tok.type in ops:
 			op_tok = self.current_tok
-			res.register(self.advance())
+			res.register_advancment()
+			self.advance()
 			right = res.register(func_b())
 			if res.error: return res
 			left = BinOpNode(left, op_tok, right)
@@ -484,6 +505,12 @@ class Number:
 		if isinstance(other, Number):
 			return Number(self.value ** other.value).set_context(self.context), None
 
+	def copy(self):
+		copy = Number(self.value)
+		copy.set_pos(self.pos_start, self.pos_end)
+		copy.set_context(self.context)
+		return copy
+
 	def __repr__(self):
 		return str(self.value)
 
@@ -552,6 +579,7 @@ class Interpreter:
 				context
 			))
 
+		value = value.copy().set_pos(node.pos_start, node.pos_end)
 		return res.success(value)
 
 	def visit_VarAssignNode(self, node, context):
